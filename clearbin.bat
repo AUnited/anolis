c19e854e5826bd1b2c26b05dea730d20f1ccbfdb
DragonZX <DragonZX@yandex.ru>
2020-06-13 12:18:07 +0300

    Fix Release making

---
+++ clearbin.bat
@@ -0,0 +1,14 @@
+rmdir /s /q Anolis.Console\bin Anolis.Console\obj
+rmdir /s /q Anolis.Core\bin Anolis.Core\obj
+rmdir /s /q Anolis.Gui\bin Anolis.Gui\obj
+rmdir /s /q Anolis.IconEditor\bin Anolis.IconEditor\obj
+rmdir /s /q Anolis.Installer\bin Anolis.Installer\obj
+rmdir /s /q Anolis.Packager\bin Anolis.Packager\obj
+rmdir /s /q Anolis.Packages\bin Anolis.Packages\obj
+rmdir /s /q Anolis.Resourcer\bin Anolis.Resourcer\obj
+rmdir /s /q Anolis.Resources\bin Anolis.Resources\obj
+rmdir /s /q Anolis.Tests\bin Anolis.Tests\obj
+rmdir /s /q Anolis.Tools.ImageTools\bin Anolis.Tools.ImageTools\obj
+rmdir /s /q Anolis.Tools.PEInfo\bin Anolis.Tools.PEInfo\obj
+rmdir /s /q Anolis.Tools.TweakUI\bin Anolis.Tools.TweakUI\obj
+pause
